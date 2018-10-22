c6d8e6bff1cbb38e95d44c416202a57335da8ec1
scfkcf <scfkcf@gmail.com>
2018-10-22 18:46:37 +0800

    init

---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#define PB push_back
+#define MP make_pair
+#define X first
+#define Y second
+using namespace std ;
+typedef long long ll;
+
+map<>
+vector<>
+
+
+/* aim: 
+	1. input new word, 
+		A. identify the group by Chinese
+		B. identify the group by one synonym
+		C. same word will ask modify first
+	2. display
+		A. input chinese, display group
+		B. input a word, display its group
+		C. display all the data, chinese first and words second
+	
+   DS: 
+*/
+
+int main() {
+	
+	return 0;
+}
+
